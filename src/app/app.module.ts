a6d1bd9ab1fa770034ae7edd7465d8849e64f257
MatthewKK01 <mateyavelashvili@gmail.com>
2024-03-13 09:19:32 +0400

    created doctor card ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DoctorCardComponent } from './components/doctor-card/doctor-card.component';
+import { UserRegistrationComponent } from './components/user-registration/user-registration.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    DoctorCardComponent,
+    UserRegistrationComponent,
 
   ],
   imports: [
