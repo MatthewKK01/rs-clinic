6a3a3bc05f31fe6a2a072559576a658e3cb7e00c
MatthewKK01 <mateyavelashvili@gmail.com>
2024-03-15 17:32:32 +0400

    created doctor registration ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserRegistrationComponent } from './components/user-registration/user-registration.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { DoctorRegistrationComponent } from './components/doctor-registration/doctor-registration.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'user', component: UserRegistrationComponent },
-  { path: 'doctor', component: UserRegistrationComponent },
+  { path: 'doctor', component: DoctorRegistrationComponent },
 ];
 
 @NgModule({
