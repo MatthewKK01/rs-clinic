9baf6359b4de89f047e33c0aa2a4bb845e065cdc
MatthewKK01 <mateyavelashvili@gmail.com>
2024-03-15 08:18:13 +0400

    created rating ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,9 @@ import { DoctorCardComponent } from './components/doctor-card/doctor-card.compon
 import { UserRegistrationComponent } from './components/user-registration/user-registration.component';
 import { ModalComponent } from './components/modal/modal.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
-
+import { DoctorRegistrationComponent } from './components/doctor-registration/doctor-registration.component';
+import { NgxStarRatingModule } from 'ngx-star-rating';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,11 +18,13 @@ import { HomepageComponent } from './components/homepage/homepage.component';
     UserRegistrationComponent,
     ModalComponent,
     HomepageComponent,
-
+    DoctorRegistrationComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxStarRatingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/doctor-card/doctor-card.component.ts
+++ src/app/components/doctor-card/doctor-card.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-doctor-card',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./doctor-card.component.scss']
 })
 export class DoctorCardComponent {
-
+  public myForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      rating: ['3', Validators.required],
+    })
+  }
 }
+
